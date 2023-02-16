83b0848de4c816f661dac306c768f54ef8962426
daanjiri <da.jimenez166@gmail.com>
2023-02-16 23:05:04 +0000

    login signin

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from backend import create_app
 from flask_restful import Api
 from .modelos import db
-from .vistas import VistaCanciones, VistaCancion, VistaSignIn, VistaLogIn, VistaAlbum, VistaAlbumsUsuario, VistaCancionesAlbum
+from .vistas import VistaSignIn, VistaLogIn
 
 app = create_app('default')
 app_context = app.app_context()
@@ -11,10 +11,10 @@ db.init_app(app)
 db.create_all()
 
 api = Api(app)
-api.add_resource(VistaCanciones, '/canciones')
-api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
+# api.add_resource(VistaCanciones, '/canciones')
+# api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
 api.add_resource(VistaSignIn, '/signin')
 api.add_resource(VistaLogIn, '/login')
-api.add_resource(VistaAlbumsUsuario, '/usuario/<int:id_usuario>/albumes')
-api.add_resource(VistaAlbum, '/album/<int:id_album>')
-api.add_resource(VistaCancionesAlbum, '/album/<int:id_album>/canciones')
+# api.add_resource(VistaAlbumsUsuario, '/usuario/<int:id_usuario>/albumes')
+# api.add_resource(VistaAlbum, '/album/<int:id_album>')
+# api.add_resource(VistaCancionesAlbum, '/album/<int:id_album>/canciones')
--- modelos/modelos.py
+++ modelos/modelos.py
@@ -6,37 +6,46 @@ import enum
 
 db = SQLAlchemy()
 
-albumes_canciones = db.Table('album_cancion',
-    db.Column('album_id', db.Integer, db.ForeignKey('album.id'), primary_key = True),
-    db.Column('cancion_id', db.Integer, db.ForeignKey('cancion.id'), primary_key = True))
+# albumes_canciones = db.Table('album_cancion',
+#                              db.Column('album_id', db.Integer, db.ForeignKey(
+#                                  'album.id'), primary_key=True),
+#                              db.Column('cancion_id', db.Integer, db.ForeignKey('cancion.id'), primary_key=True))
 
-class Cancion(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    titulo = db.Column(db.String(128))
-    minutos = db.Column(db.Integer)
-    segundos = db.Column(db.Integer)
-    interprete = db.Column(db.String(128))
-    albumes = db.relationship('Album', secondary = 'album_cancion', back_populates="canciones")
 
-class Medio(enum.Enum):
-   DISCO = 1
-   CASETE = 2
-   CD = 3
+# class Cancion(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     titulo = db.Column(db.String(128))
+#     minutos = db.Column(db.Integer)
+#     segundos = db.Column(db.Integer)
+#     interprete = db.Column(db.String(128))
+#     albumes = db.relationship(
+#         'Album', secondary='album_cancion', back_populates="canciones")
 
-class Album(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(128))
-    anio = db.Column(db.Integer)
-    descripcion = db.Column(db.String(512))
-    medio = db.Column(db.Enum(Medio))
-    usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"))
-    canciones = db.relationship('Cancion', secondary = 'album_cancion', back_populates="albumes")
-    
+
+# class Medio(enum.Enum):
+#     DISCO = 1
+#     CASETE = 2
+#     CD = 3
+
+
+# class Album(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     titulo = db.Column(db.String(128))
+#     anio = db.Column(db.Integer)
+#     descripcion = db.Column(db.String(512))
+#     medio = db.Column(db.Enum(Medio))
+#     usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"))
+#     canciones = db.relationship(
+#         'Cancion', secondary='album_cancion', back_populates="albumes")
+
+
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50))
     contrasena = db.Column(db.String(50))
-    albumes = db.relationship('Album', cascade='all, delete, delete-orphan')
+    email = db.Column(db.String(80), unique=True)
+    # albumes = db.relationship('Album', cascade='all, delete, delete-orphan')
+
 
 class EnumADiccionario(fields.Field):
     def _serialize(self, value, attr, obj, **kwargs):
@@ -44,21 +53,24 @@ class EnumADiccionario(fields.Field):
             return None
         return {"llave": value.name, "valor": value.value}
 
-class CancionSchema(SQLAlchemyAutoSchema):
-    class Meta:
-         model = Cancion
-         include_relationships = True
-         load_instance = True
 
-class AlbumSchema(SQLAlchemyAutoSchema):
-    medio = EnumADiccionario(attribute=("medio"))
-    class Meta:
-         model = Album
-         include_relationships = True
-         load_instance = True
+# class CancionSchema(SQLAlchemyAutoSchema):
+#     class Meta:
+#         model = Cancion
+#         include_relationships = True
+#         load_instance = True
+
+
+# class AlbumSchema(SQLAlchemyAutoSchema):
+#     medio = EnumADiccionario(attribute=("medio"))
+
+#     class Meta:
+#         model = Album
+#         include_relationships = True
+#         load_instance = True
 
 class UsuarioSchema(SQLAlchemyAutoSchema):
     class Meta:
-         model = Usuario
-         include_relationships = True
-         load_instance = True
+        model = Usuario
+        include_relationships = True
+        load_instance = True
